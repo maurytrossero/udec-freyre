ec130d28ac5dae307222c99c561984989026a2d9
Mauricio <maurytrossero@gmail.com>
2019-05-06 21:04:40 -0300

    cuarto_commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -30,10 +30,14 @@ class DatabaseSeeder extends Seeder
     public function TruncateTables(array $tables): void
     {
         foreach ($tables as $table)
-        {
+        {/*
             DB::statement('SET FOREIGN_KEY_CHECKS = 0');
             DB::table($table)->truncate();
             DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+        */
+            DB::statement($this->getDisableStatement());
+            DB::table($table)->truncate();
+            DB::statement($this->getEnableStatement());
         }
 
     }

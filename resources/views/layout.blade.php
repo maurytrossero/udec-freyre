98ce6565533c116805265d3636cd80e5cc5a7af2
Mauricio <maurytrossero@gmail.com>
2024-04-16 11:50:53 -0300

    layout fix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
 
 
-
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="Course Project">
     <meta name="viewport" content="width=device-width, initial-scale=1">
